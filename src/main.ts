3cd6da6b1a117521b376a91915d58f78388fa89b
Willkommen <turash.chowdhury@gmail.com>
2022-10-02 17:40:33 +0600

    env mod

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import axios from 'axios';
 
 import './assets/main.css'
 
-// axios.defaults.baseURL = 'https://swe-b-dev.ebuilders.pro/api/';
-axios.defaults.baseURL = 'http://localhost:8080/api/';
+axios.defaults.baseURL = 'https://swe-b2-dev.ebuilders.pro/api/';
+// axios.defaults.baseURL = 'http://localhost:8080/api/';
 axios.defaults.headers.common['Authorization']= `Bearer ${localStorage.getItem('token')}`;
 axios.defaults.withCredentials = true;
 
